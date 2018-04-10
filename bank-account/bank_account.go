56905d957c634e42fe70a8c8faa0a0b7a4aabccd
Thomas McNicol <thomas.mcnicol@wspdigital.com>
2018-04-10 16:08:27 +1000

    Uses channel to guard memory instead of a mutex
    This is actually a slower implementation.

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -1,59 +1,89 @@
 package account
 
-import "sync"
-
 type Account struct {
 	balance int64
 	closed  bool
-	mu      *sync.Mutex
+	actionc chan func()
+	quitc   chan chan struct{}
 }
 
 func Open(amt int64) *Account {
 	if amt < 0 {
 		return nil
 	}
-	return &Account{
+
+	a := &Account{
 		balance: amt,
 		closed:  false,
-		mu:      &sync.Mutex{},
+		actionc: make(chan func()),
+		quitc:   make(chan chan struct{}),
+	}
+	go a.loop()
+	return a
+}
+
+func (a *Account) loop() {
+	for {
+		select {
+		case f := <-a.actionc:
+			f()
+		case q := <-a.quitc:
+			close(q)
+			return
+		}
 	}
 }
 
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-	if a.closed {
-		return 0, false
+	b := make(chan int64)
+	okc := make(chan bool)
+
+	a.actionc <- func() {
+		if a.closed {
+			b <- 0
+			okc <- false
+		} else {
+			b <- a.balance
+			okc <- true
+		}
 	}
-	return a.balance, true
+	return <-b, <-okc
+
 }
+
 func (a *Account) Close() (payout int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
+	p := make(chan int64)
+	okc := make(chan bool)
 
-	if a.closed {
-		return 0, false
+	a.actionc <- func() {
+		if a.closed {
+			p <- 0
+			okc <- false
+		} else {
+			p <- a.balance
+			// Update the balance
+			a.balance = 0
+			a.closed = true
+			okc <- true
+		}
 	}
-
-	balance := a.balance
-
-	// Update the account details.
-	a.balance = 0
-	a.closed = true
-
-	return balance, true
+	return <-p, <-okc
 }
+
 func (a *Account) Deposit(amt int64) (newbalance int64, ok bool) {
-	a.mu.Lock()
-	defer a.mu.Unlock()
+	nb := make(chan int64)
+	okc := make(chan bool)
 
-	if a.balance < -amt {
-		return 0, false
-	}
+	a.actionc <- func() {
+		if a.balance+amt < 0 || a.closed {
+			nb <- 0
+			okc <- false
+		} else {
+			a.balance += amt
 
-	if a.closed {
-		return 0, false
+			nb <- a.balance
+			okc <- true
+		}
 	}
-	a.balance += amt
-	return a.balance, true
+	return <-nb, <-okc
 }
